570095888b62098f29b13ce72ce0be44fb75fb2c
Tessa Murray <tmurray@bsd220.org>
2024-11-21 07:35:16 -0800

    Initial Commit

---
+++ Tic Tac Toe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tic Tac Toe
+//
+//  Created by Tessa Murray on 11/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
